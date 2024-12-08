1010867efcaa6befafb7d9776f5aa3f23f57318a
zyerin <zyerin5974@gmail.com>
2024-12-09 00:57:10 +0900

    feat: Dockerfile 생성

--- src/test/java/com/example/user_service/UserServiceApplicationTests.java
+++ src/test/java/com/example/user_service/UserServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.user_service;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
