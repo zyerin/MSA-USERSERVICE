327d10f1aea754a6dbc7637129e0d0f0e5907616
kimjungho <kwjdgh1267@gmail.com>
2024-12-08 20:34:37 +0900

    Feat: deploy.yml changed
    merge시 ci/cd 발생하도록 설정

--- src/main/java/com/example/user_service/entity/RedisMember.java
+++ src/main/java/com/example/user_service/entity/RedisMember.java
@@ -2,6 +2,7 @@ package com.example.user_service.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.core.RedisHash;
 @Setter
 @RedisHash(value = "redismember", timeToLive = 600)//단위는 초임
 @AllArgsConstructor
+@NoArgsConstructor
 public class RedisMember {
     @Id
     private String id;
